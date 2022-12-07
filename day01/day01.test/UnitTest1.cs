975296f976ccd3613b19d70da155d1c6536ca0b9
Alex Mickelson <alexmickelson96@gmail.com>
2022-12-06 20:30:48 -0700

    started day 01, created newday script

---
+++ day01/day01.test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace day01.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ newDay.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+name=$1
+className=$name
+testName="$name.test"
+mainName="$name.main"
+
+mkdir "$name"
+cd $name
+
+echo "Creating project with $name"
+
+mkdir $className
+mkdir $testName
+mkdir $mainName
+
+cd $className
+dotnet new console
+cd "../$testName"
+dotnet new nunit
+dotnet add reference "../$className"
+cd "../$mainName"
+dotnet new console
+dotnet add reference "../$className"
+cd ..
+
+dotnet new sln
+dotnet sln add $className
+dotnet sln add $testName
+dotnet sln add $mainName
